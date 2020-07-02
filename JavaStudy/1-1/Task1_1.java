145fe5ac99e44dc16dc673b3b84820dd4a79ef6f
Yuuki-Kubota <kubota.20180402@gmail.com>
2020-07-02 23:51:16 +0900

    課題1_1 データ型・文字列クラス

---
+++ JavaStudy/1-1/Task1_1.java
@@ -0,0 +1,35 @@
+/**
+ * Task1_1: 課題内容
+ *
+ * 本課題では、データ型・文字列の使い方を学んでいきましょう。
+ * 問①から問⑧まであります。
+ * 指定された型・値・変数名を守って記述してください。
+ * 問③,⑦については、コメントを入れてください。
+ *
+ */
+public class Task1_1 {
+
+    public static void main(String[] args) {
+
+         // ①int型の hogeInt という名前の変数を宣言し、値「50」で初期化
+         int hogeInt=50;
+         // ②Stringクラスの hogeString という名前の変数を宣言、値「テスト」で初期化
+         String hogeString="テスト";
+         // ③下記の処理について、何をしているのかコメントを記入してください
+         // [double型のhogeDoubleという名前の変数を宣言、値「テスト」で初期か]
+         double hogeDouble = 0.2;
+         // ④変数 hogeInt を値「100」で上書き
+          hogeInt=100;
+         // ⑤変数 hogeString を値「合格」で上書き
+         hogeString="合格";
+         // ⑥変数 hogeInt の値を表示
+         System.out.println(hogeInt);
+         // ⑦下記の処理について、何をしているのかコメントを記入してください
+         // [printlnメゾットでhogeStringの値を表示させている]
+          System.out.println(hogeString);
+          // ⑧変数 hogeDouble の値を表示
+          System.out.println(hogeDouble);
+
+
+    }
+}
