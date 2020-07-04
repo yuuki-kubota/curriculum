5b6d66c98424309112a2847110d966ec25b55fee
Yuuki-Kubota <kubota.20180402@gmail.com>
2020-07-05 01:00:48 +0900

    課題1_3　演算子

---
+++ JavaStudy/1-3/Task1_3.java
@@ -0,0 +1,34 @@
+
+public class Task1_3 {
+
+    public static void main(String[] args) {
+
+        // ① 5 と 10 を加算した値を代入する、変数 plus を作成しなさい。
+        int plus=10 + 5;
+
+        // ② 20 から 7 を減算した値を代入する、変数 minus を作成しなさい。
+        int minus=20 - 7;
+
+        // ③下記の処理について、何をしているのかコメントを記入してください。
+        // [10から2を乗算した値を代入する、変数 mltiply を作成しなさい]
+        int multiply = 10 * 2;
+
+        // ④ 20 を 6 で割った余りを代入する、変数 remi を作成しなさい。
+        int remi=20 % 6;
+
+        // ⑤下記の処理について、何をしているのかコメントを記入してください
+        // [ここへ記述]
+        String town = "有楽町";
+        String line = "線";
+
+        // ⑥ ⑤で作成した変数を連結させた値を代入する変数 train を作成しなさい。
+        String train= "有楽町" + "線";
+
+        // ⑦ 変数plus, minus, multiply, remi, train をそれぞれ出力しなさい。
+        System.out.println(plus);
+        System.out.println(minus);
+        System.out.println(multiply);
+        System.out.println(remi);
+
+    }
+}
